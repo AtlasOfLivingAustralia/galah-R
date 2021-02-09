43e9300bf81155e5b1ee73eb098a8e37c904ef64
Martin Westgate <martinjwestgate@gmail.com>
2021-02-09 11:06:38 +1100

    add new function ala_species, based on earlier species checklist function
    Also minor update to galah help file

--- R/galah.R
+++ R/galah.R
@@ -1,16 +1,20 @@
 #' \pkg{galah}
 #'
-#' This project enables the R community to access data and tools hosted by the
-#' Atlas of Living Australia. The goal of the project is
-#' to enable basic species and related information to be queried and output in
-#' standard formats for R. galah is based around the extensive web services
-#' provided by the Atlas; see the API link below.
+#' \code{galah} is an R interface to biodiversity data hosted by the
+#' Atlas of Living Australia (\url{https://www.ala.org.au/}). It enables
+#' users to locate and download species observations, taxonomic information,
+#' or associated media such images or sounds, and to restrict their queries
+#' to particular taxa or locations. Users can specify precisely what
+#' columns are returned by each query, or restrict their results to observations
+#' that meet particular quality-control criteria. All functions return a
+#' \code{data.frame} as their standard format.
 #'
-#' If you have any questions or suggestions, please email support@ala.org.au.
+#'
+#' If you have any questions, comments or suggestions, please email \href{mailto:support@ala.org.au}{support@ala.org.au}.
 #'
 #' @name galah
 #' @docType package
-#' @references \url{https://api.ala.org.au/}
+#' @references For more information on the ALA API, visit \url{https://api.ala.org.au/}
 #' @import assertthat sp sf httr
 #' @importFrom crul HttpClient Paginator
 #' @importFrom digest digest
@@ -21,17 +25,18 @@
 #' @importFrom utils data packageVersion read.csv write.csv read.table str
 #' unzip URLencode download.file setTxtProgressBar txtProgressBar tail
 #' @importFrom wellknown lint
-#' 
+#'
 #' @section Functions:
 #' \strong{Data}
 #' \itemize{
-#'   \item\code{\link{ala_taxa}} Taxon information
 #'   \item\code{\link{ala_occurrences}} Download occurrence records
+#'   \item\code{\link{ala_species}} Download occurrence records
 #'   \item\code{\link{ala_counts}} Summary statistics for occurrence records
 #'   \item\code{\link{ala_media}} Download images and sounds
 #' }
 #' \strong{Filter}
 #' \itemize{
+#'   \item\code{\link{select_taxa}} Taxon information
 #'   \item\code{\link{select_filters}} Filter records
 #'   \item\code{\link{select_locations}} Specify location
 #'   \item\code{\link{select_columns}} Columns to return in occurrence download
@@ -45,7 +50,7 @@
 #'   \item\code{\link{find_layers}} List spatial layers
 #'   \item\code{\link{find_reasons}} List valid download reasons
 #' }
-#' \strong{Helper}
+#' \strong{Help}
 #' \itemize{
 #'   \item\code{\link{ala_config}} Package configuration options
 #'   \item\code{\link{ala_citation}} Citation for a dataset
