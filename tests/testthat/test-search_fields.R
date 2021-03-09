a75a701a9778f988caa9325971048c9bdfef135c
matilda2812 <matildastevenson2812@gmail.com>
2021-03-09 13:19:57 +1100

    Add tests for search_fields

---
+++ tests/testthat/test-search_fields.R
@@ -0,0 +1,14 @@
+context("Test search_fields")
+
+test_that("search_fields returns correct types of field", {
+  expect_setequal(unique(search_fields(type = "all")$type),
+               c("fields", "assertions", "layers"))
+  expect_equal(unique(search_fields(type = "assertions")$type),
+               "assertions")
+})
+
+test_that("search_fields searches text correctly", {
+  expect_true(all(grepl(pattern = "precipitation",
+                        search_fields("precipitation")$description,
+                        ignore.case = TRUE)))
+})
