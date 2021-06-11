86d76e817fa854003fd4e42f8c5a4bb96111088a
matildastevenson <matildastevenson2812@gmail.com>
2021-06-11 16:09:08 +1000

    Import url_build

--- R/galah.R
+++ R/galah.R
@@ -80,7 +80,7 @@
 #' @docType package
 #' @references For more information on the ALA API, visit \url{https://api.ala.org.au/}
 #' @import assertthat sp sf httr
-#' @importFrom crul HttpClient Paginator Async
+#' @importFrom crul HttpClient Paginator Async url_build
 #' @importFrom digest digest
 #' @importFrom jsonlite fromJSON
 #' @importFrom stringr regex str_c str_detect str_extract str_locate
