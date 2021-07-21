b57f5dd737b585edce754a90590f2b24974829ef
matildastevenson <matildastevenson2812@gmail.com>
2021-07-21 12:45:51 +1000

    Skip piping test on travis

--- tests/testthat/test-data_request.R
+++ tests/testthat/test-data_request.R
@@ -23,6 +23,7 @@ test_that("data request returns correct data_request object", {
 
 test_that("data request returns correct data_request object when piping", {
   skip_on_cran()
+  skip_on_travis()
   taxa <- "Polytelis swainsonii"
   req <- data_request() |> select_taxa(taxa)
   expect_equal(req$taxa, select_taxa(taxa))
