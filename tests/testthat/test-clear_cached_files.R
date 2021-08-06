d1a16755ecd3437cf57edef747f07efaa7b05d76
Dax Kellie <Dax.Kellie@csiro.au>
2021-08-06 11:56:16 +1000

    Build package tests

--- tests/testthat/test-clear_cached_files.R
+++ tests/testthat/test-clear_cached_files.R
@@ -2,8 +2,9 @@ context("Test deletion of cached files and cached file metadata")
 
 test_that("clear_cached_files gives an error if no metadata file exists", {
   skip_on_cran()
-  ala_config(cache_directory = tempdir())
-  # make sure there is no metadata file in directorys
+  galah_config(caching = TRUE, cache_directory = tempdir())
+  # make sure there is no metadata file in directories
   suppressWarnings(file.remove(file.path(tempdir(), 'metadata.rds')))
   expect_message(clear_cached_files())
+  galah_config(caching = FALSE)
 })
