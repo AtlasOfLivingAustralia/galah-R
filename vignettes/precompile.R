471956d1efd8600ece3b495fde224e312bd2a151
Dax Kellie <Dax.Kellie@csiro.au>
2021-08-11 13:15:28 +1000

    Precompile taxonomic_information vignette #85
    * Create taxonomic_information.Rmd.orig file
    * Add vignette plots to vignettes/ folder

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -4,3 +4,5 @@ library(knitr)
 knit("vignettes/galah.Rmd.orig", "vignettes/galah.Rmd")
 knit("vignettes/international_atlases.Rmd.orig",
      "vignettes/international_atlases.Rmd")
+knit("vignettes/taxonomic_information.Rmd.orig",
+     "vignettes/taxonomic_information.Rmd")
