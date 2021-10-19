183ce81353f037e2921df397c3f74314d27f77e5
Martin Westgate <martinjwestgate@gmail.com>
2021-10-19 15:30:51 +1100

    Only set `galah_config(valid_fields) on first use of `validate_fields` (last part of fix for #73)
    Minor change to previous commit; but increases package load times.

--- R/onload.R
+++ R/onload.R
@@ -1,8 +1,5 @@
 .onLoad <- function(libname, pkgname) {
     if (pkgname == "galah") {
       galah_config() ## will set to default values if not already set
-      galah_config(
-        valid_fields = sort(unique(c(search_fields()$id, all_fields()$name)))
-      )
     }
 }
