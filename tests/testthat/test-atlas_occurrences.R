1d2fac7acb0ff9c92ad47c854977f8ab87f28df5
Dax Kellie <dax.kellie@csiro.au>
2021-12-14 12:44:36 +1100

    Rename and update test files to new syntax (#107)

---
+++ tests/testthat/test-atlas_occurrences.R
@@ -0,0 +1,69 @@
+context("Get occurrence data")
+
+vcr::use_cassette("atlas_occurrences_no_filters", {
+  test_that("atlas_occurrences doesn't allow large downloads", {
+    expect_error(atlas_occurrences())
+  })
+})
+
+test_that("atlas_occurrences gives a nice error for invalid emails", {
+  skip_on_cran()
+  galah_config(email = "test@test.org.au")
+  expect_error(atlas_occurrences(taxa = select_taxa("Thylacinus cynocephalus")))
+  galah_config(email = "test@test.org.au")
+})
+
+test_that("atlas_occurrences fails nicely if no email is provided", {
+  galah_config(email = "", run_checks = FALSE)
+  expect_error(atlas_occurrences(filter = galah_filter(year == 1900)))
+  galah_config(email = "ala4r@ala.org.au")
+})
+  
+
+  # test all filters and type of columns in one call
+test_that("atlas_occurrences returns requested columns", {
+  skip_on_cran()
+  expected_cols <- c("decimalLatitude", "decimalLongitude", "eventDate",
+                     "scientificName", "taxonConceptID", "recordID",
+                     "dataResourceName", "stateProvince", "ZERO_COORDINATE")
+  filters <- galah_filter(year == seq(2018, 2020))
+  cols <- galah_select(group = "basic", "stateProvince", "ZERO_COORDINATE")
+  taxa <- select_taxa("Polytelis swainsonii")
+  poly <- "POLYGON((146.7 -34.6,147.9 -34.6,147.9 -35.7,146.7 -35.7,146.7 -34.6))"
+  locations <- galah_location(poly)
+  occ <- atlas_occurrences(
+    taxa = taxa,
+    filter = filters,
+    select = cols,
+    location = locations)
+  expect_setequal(names(occ), expected_cols)
+  expect_equal(unique(occ$stateProvince), "New South Wales")
+})
+
+test_that("atlas_occurrences caches data as expected", {
+  skip_on_cran()
+  galah_config(caching = TRUE, verbose = TRUE)
+  taxa <- select_taxa("Wurmbea dioica")
+  filters <- galah_filter(year == 2000)
+  columns <- galah_select(group = "basic", basisOfRecord)
+
+  # Download data
+  occ <- atlas_occurrences(taxa = taxa, filter = filters, select = columns)
+  # Re-download data
+  expect_message(
+    atlas_occurrences(taxa = taxa, filter = filters, select = columns), 
+    "Using cached file")
+  galah_config(caching = FALSE)
+})
+
+test_that("atlas_occurrences downloads data from a DOI", {
+  skip_on_cran()
+    doi <- "10.26197/ala.0c1e8744-a639-47f1-9a5f-5610017ba060"
+    occ <- atlas_occurrences(doi = doi)
+    expect_s3_class(occ, "data.frame")
+    expect_gt(nrow(atlas_occurrences(doi = doi)), 0)
+})
+
+test_that("atlas_occurrences checks DOI provided", {
+  expect_error(atlas_occurrences(doi = "random_doi"))
+})
--- tests/testthat/test-select_filters.R
+++
@@ -1,3 +0,0 @@
-context("Test select_filters")
-
-# TODO: Test that select_filters returns suggestion to use galah_filter()
