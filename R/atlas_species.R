ffa4569af3066f6be1e051938e815280e123ec17
Dax Kellie <dax.kellie@csiro.au>
2021-12-16 11:28:38 +1100

    Update syntax in `atlas_species` tests
    * Minor updates to documentation

--- R/atlas_species.R
+++ R/atlas_species.R
@@ -1,7 +1,7 @@
 #' Return species lists
 #'
 #' While there are reasons why users may need to check every record meeting their
-#' search criteria (i.e. using \code{\link{atlas_occurrences}}), a common use case
+#' search criteria (i.e. using \code{\link{atlas_occurrences()}}), a common use case
 #' is to simply identify which species occur in a specified region, time period,
 #' or taxonomic group. This function returns a \code{data.frame} with one row
 #' per species, and columns giving associated taxonomic information.
@@ -15,27 +15,29 @@
 #' (i.e., taxa, filter, geolocate, columns)
 #' @details
 #' The primary use case of this function is to extract species-level information
-#' given a set of criteria defined by \code{\link{select_taxa}()},
+#' given a set of criteria defined by \code{\link{search_taxa}()},
 #' \code{\link{galah_filter}()} or \code{\link{galah_geolocate}()}. If the purpose
 #' is simply to get taxonomic information that is not restricted by filtering,
-#' then \code{\link{select_taxa}()} is more efficient. Similarly, if counts are
+#' then \code{\link{search_taxa}()} is more efficient. Similarly, if counts are
 #' required that include filter but without returning taxonomic detail, then
 #' \code{\link{atlas_counts}()} is more efficient (see examples).
 #' @examples \dontrun{
 #'
 #' # Look up genus "Heleioporus" in the ALA
-#' select_taxa("Heleioporus")
+#' search_taxa("Heleioporus")
 #'
 #' # Find how many records there are for this genus
-#' atlas_counts(select_taxa("Heleioporus"))
+#' atlas_counts(search_taxa("Heleioporus"))
 #'
 #' # Get taxonomic information on all species within this genus
 #' # (every row is a species with associated taxonomic data)
-#' atlas_species(select_taxa("Heleioporus"))
+#' atlas_species(search_taxa("Heleioporus"))
 #' }
 #' @export
-atlas_species <- function(taxa = NULL, filter = NULL, geolocate = NULL,
-                        refresh_cache = FALSE) {
+atlas_species <- function(taxa = NULL, 
+                          filter = NULL, 
+                          geolocate = NULL,
+                          refresh_cache = FALSE) {
   # check whether species download is possible
   species_url <- server_config("species_base_url")
 
--- tests/testthat/test-atlas_species.R
+++ tests/testthat/test-atlas_species.R
@@ -3,12 +3,12 @@ context("Test atlas_species")
 test_that("atlas_species returns a dataframe", {
   skip_on_cran()
   galah_config(caching = TRUE)
-  species <- atlas_species(taxa = select_taxa("Osphranter"))
+  species <- atlas_species(taxa = search_taxa("Osphranter"))
   expect_s3_class(species, "data.frame")
   expect_gt(nrow(species), 1)
   
   # check cached results is the same
-  expect_message(species2 <- atlas_species(taxa = select_taxa("Osphranter")))
+  expect_message(species2 <- atlas_species(taxa = search_taxa("Osphranter")))
   expect_equal(species, species2)
   galah_config(caching = FALSE)
 })
