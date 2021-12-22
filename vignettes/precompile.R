775f22a4c07735359681462b7c8b7a1184e456c6
Martin Westgate <martinjwestgate@gmail.com>
2021-12-22 11:17:20 +1100

    continue updating vignettes to new syntax

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -4,7 +4,8 @@ library(galah)
 galah_config(email = "email@email.com") # add your email
 
 library(knitr)
-knit("vignettes/galah.Rmd.orig", "vignettes/galah.Rmd")
+knit("vignettes/galah.Rmd.orig", 
+     "vignettes/galah.Rmd")
 knit("vignettes/international_atlases.Rmd.orig",
      "vignettes/international_atlases.Rmd")
 knit("vignettes/taxonomic_information.Rmd.orig",
