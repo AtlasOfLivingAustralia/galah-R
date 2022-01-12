758291cae56e204d31d2867461c58690baa4450b
Martin Westgate <martinjwestgate@gmail.com>
2022-01-12 17:27:01 +1100

    first attempt at rebuilding vignettes in a prettier way
    These don't render well yet, but group earlier information in a new way

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -4,11 +4,26 @@ library(galah)
 galah_config(email = "email@email.com") # add your email
 
 library(knitr)
-knit("vignettes/galah.Rmd.orig", 
-     "vignettes/galah.Rmd")
-knit("vignettes/international_atlases.Rmd.orig",
-     "vignettes/international_atlases.Rmd")
-knit("vignettes/taxonomic_information.Rmd.orig",
-     "vignettes/taxonomic_information.Rmd")
-knit("vignettes/revamped_syntax.Rmd.orig",
-     "vignettes/revamped_syntax.Rmd")
+# knit("vignettes/galah.Rmd.orig", 
+#      "vignettes/galah.Rmd")
+# knit("vignettes/international_atlases.Rmd.orig",
+#      "vignettes/international_atlases.Rmd")
+# knit("vignettes/taxonomic_information.Rmd.orig",
+#      "vignettes/taxonomic_information.Rmd")
+# knit("vignettes/revamped_syntax.Rmd.orig",
+#      "vignettes/revamped_syntax.Rmd")
+
+# workflow for automated detection and processing of vignettes
+folder <- "./vignettes/"
+all_files <- list.files(folder)
+selected_files <- paste0(folder, all_files[grepl(".orig$", all_files)])
+out_files <- sub(".orig$", "", selected_files)
+
+lapply(seq_along(selected_files), function(a){knit(
+  selected_files[[a]],
+  out_files[[a]]
+)})
+
+# # to render to HTML:
+# devtools::build_vignettes()
+# pkgdown::build_site()
