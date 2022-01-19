b0826d5280bc5607019327305f48adb25408011a
Martin Westgate <martinjwestgate@gmail.com>
2022-01-19 13:31:26 +1100

    add `vcr` for `galah_identify` tests
    prevents CRAN checks failing if the ALA is down

--- tests/testthat/test-galah_identify.R
+++ tests/testthat/test-galah_identify.R
@@ -6,20 +6,21 @@ test_that("galah_identify returns an empty tibble when no args provided", {
 })
 
 test_that("galah_identify runs a search when given a string", {
-  result <- galah_identify("Litoria_peronii")
+  vcr::use_cassette("galah_identify_search_1", {
+    result <- galah_identify("Litoria_peronii")
+  })
   expect_equal(nrow(result), 1)
 })
 
 test_that("galah_identify runs a search on multiple strings", {
-  result <- galah_identify("amphibia", "reptilia", "aves", "mammalia")
+  vcr::use_cassette("galah_identify_search_2", {
+    result <- galah_identify("amphibia", "reptilia", "aves", "mammalia")
+  })
   expect_equal(nrow(result), 4)
 })
 
-# test_that("galah_identify drops unknown rows", {
-#   expect_warning(galah_identify("amphibia", "reptilia", "aves", "nothing"))
-# })
-
 test_that("galah_identify works with search = FALSE", {
+  galah_config(run_checks = FALSE)
   id <- "urn:lsid:biodiversity.org.au:afd.taxon:0490a9ba-0d08-473d-a709-6c42e354f118"
   result <- galah_identify(id, search = FALSE)
   expect_equal(result$identifier[1], id)
@@ -36,13 +37,14 @@ test_that("galah_identify can pass a string unchanged when run_checks = FALSE", 
   result <- galah_identify("a_string", search = FALSE)
   expect_equal(nrow(result), 1)
   expect_equal(result$identifier[1], "a_string")
-  galah_config(run_checks = TRUE)
 })
 
 test_that("galah_identify pipes correctly", {
-  result <- galah_call() |>
-    galah_identify("Litoria") |>
-    galah_filter(year == 2020) 
+  vcr::use_cassette("galah_identify_search_3", {
+    result <- galah_call() |>
+      galah_identify("Litoria") |>
+      galah_filter(year == 2020) 
+  })
   expect_false(is.null(result$identify))
   expect_false(is.null(result$filter)) 
 })
