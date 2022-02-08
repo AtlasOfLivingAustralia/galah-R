eb597bbd0a36065c9a55c8e19215c5bedef6f3c1
Martin Westgate <martinjwestgate@gmail.com>
2022-02-08 16:47:31 +1100

    Add `show_all_values` and `search_values` (#131)
    Slightly messy code, but should show proof of concept

---
+++ R/search_values.R
@@ -0,0 +1,31 @@
+#' Search for a value within a field
+#'
+#' Descriptive text goes here.
+#'
+#' @param field `string`: field to return the categories for. Use
+#' [search_fields()] to view valid fields.
+#' @param query `string`: A search string. Not case sensitive.
+#' @export
+
+search_values <- function(field, query){
+
+  if (missing(field) || is.null(field)) {
+    bullets <- c(
+      "We didn't detect a field to search for.",
+      i = "Try entering text to search within a given field.",
+      i = "To see all valid fields, use `show_all_fields()`."
+    )
+    rlang::warn(message = bullets, error = rlang::caller_env())
+  }
+  
+   if (missing(query) || is.null(query)) {
+     bullets <- c(
+       "We didn't detect a valid query.",
+       i = "Try entering text to search for matching values."
+     )
+     rlang::warn(message = bullets, error = rlang::caller_env())
+   }
+   
+   field_text <- show_all_values(field)
+   field_text[grepl(query, tolower(field_text$category)), ]
+}
