bb6569db16e9f33bbb5e441797d023524e6d4708
Martin Westgate <martinjwestgate@gmail.com>
2022-08-31 13:40:55 +1000

    switch to csv for storing API configurations
    Previous list version was harder to check and generalise, especially once there are many atlases (#126). New version stores the object internally, and makes it available to users via `show_all_apis`. New approach is to call `atlas_url` to build the url for you. Note that APIs combine `base_url` and `path` from previous versions.

---
+++ R/atlas_url.R
@@ -0,0 +1,17 @@
+atlas_url <- function(api_name, error_call = caller_env()){
+  
+  # run a check that a row in `node_config` matches your specification
+  current_atlas <- getOption("galah_config")$atlas
+  url_lookup <- node_config$api_name == api_name &
+                node_config$atlas == current_atlas
+  
+  if(any(url_lookup)){
+    node_config$api_url[which(url_lookup)[1]]
+  }else{
+    bullets <- c(
+      glue("The `{api_name}` API is not available for the selected atlas ({current_atlas})"),
+      i = "Use `show_all_apis()` to list valid API calls"
+    )
+    abort(bullets, call = error_call)
+  }
+}
