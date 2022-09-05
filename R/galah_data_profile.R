ff70e01ab9e745db9a4f7961b0d9faa0cd135aa1
Dax Kellie <dax.kellie@csiro.au>
2022-09-05 17:57:37 +1000

    Change to `galah_apply_profile` #130
    * Naming changed to remain consistent with verbing language of `galah_` functions

--- R/galah_data_profile.R
+++ R/galah_data_profile.R
@@ -1,14 +1,27 @@
-#' Specify a data profile
+#' Apply a data quality profile
 #'
-#' 'profiles' are groups of filters that are pre-applied by the ALA, allowing
-#' rapid narrowing of results to the most relevant data. Note that only one
-#' profile can be loaded at a time; if multiple profiles are given, the first
-#' valid profile is used.
+#' A 'profile' is a group of filters that are pre-applied by the ALA. Using a 
+#' data profile allows a query to be filtered quickly to the most relevant or 
+#' quality-assured data that is fit-for-purpose. For example, the "ALA" profile
+#' is designed to exclude lower quality records, whereas other profiles apply 
+#' filters specific to species distribution modelling (e.g. CDSM).
+#' 
+#' Note that only one profile can be loaded at a time; if multiple profiles are 
+#' given, the first valid profile is used.
+#' 
+#' For more bespoke editing of filters within a profile, use [galah_filter()]
 #'
-#' @param ... a profile name. Valid values can be seen using `show_all_profiles()$shortName`
+#' @param ... a profile name. Should be a `string` - the name or abbreviation 
+#'    of a data quality profile to apply to the query. Valid values can be seen 
+#'    using `show_all(profiles)`
+#' @return A tibble containing a valid data profile value.
+#' @seealso [show_all()] and [search_all()] to look up available data profiles. 
+#' [galah_filter()] can be used for more bespoke editing of individual data 
+#' profile filters.
+#' 
 #' @export
 
-galah_data_profile <- function(...){
+galah_apply_profile <- function(...){
   
   dots <- enquos(..., .ignore_empty = "all")
   check_filter(dots)
