c8b97262b2ca17f295496d4bc000ee37068f23f3
Martin Westgate <martinjwestgate@gmail.com>
2022-12-07 16:48:16 +1100

    Simple system for building predicates for the GBIF download system (#172)
    Takes an input from `galah_filter` and returns a JSON to pass to GBIF

---
+++ R/build_predicates.R
@@ -0,0 +1,54 @@
+#' (currently internal) function to create 'predicates'
+#' @importFrom jsonlite toJSON unbox
+
+# JSON for passing to GBIF offline downloads API.
+# https://www.gbif.org/developer/occurrence
+
+# note: GBIF requires a password as well as a username;
+# might be worth considering what system ALA will require soon,
+# and integrating both approaches.
+
+build_predicates <- function(df){ # where df is returned by galah_filter()
+  
+  send_notification <- getOption("galah_config")$send_email |> 
+    as.character() |> 
+    tolower()
+
+  data_list <- list(
+    creator = "test", # getOption("galah_config")$username, # not enabled yet - requires changes to galah_config()
+    notificationAddresses = getOption("galah_config")$email,
+    sendNotification = send_notification,
+    format = "SIMPLE_CSV",
+    predicate = list(
+      type = "and",
+      predicates = parse_predicates(df)
+    )
+  )
+  
+  toJSON(data_list)
+}
+
+# parse galah_filter result into predicate format
+# test object:
+# df <- galah_filter(year == 2010, basisOfRecord == "PRESERVED_SPECIMEN")
+parse_predicates <- function(df){
+  lapply(
+    split(df, seq_len(nrow(df))),
+    function(a){
+      list(
+        type = unbox(switch(a$logical,
+                      "==" = "equals",
+                      "<" = "lessThan",
+                      "<=" = "lessThanOrEquals",
+                      ">" = "greaterThan",
+                      ">=" = "greaterThanOrEquals",
+                      "!=" = "not"
+                      )),
+        key = unbox(a$variable),
+        value = unbox(a$value)
+      )
+    })
+}
+# NOTE: currently missing: `within` (geolocate), `or`, `in`, `isNull`, `isNotNull`
+
+
