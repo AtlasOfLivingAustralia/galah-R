0c30cb09db821c73ffa507646fa86f2a3c15fd2b
Martin Westgate <martinjwestgate@gmail.com>
2023-08-24 14:28:53 +1000

    Fix bug in `atlas_species()` leading to return of occurrences
    Issue was that `request$type` was not set properly. Uncommented FIXME in tests as well

--- R/atlas_species.R
+++ R/atlas_species.R
@@ -59,6 +59,13 @@ atlas_species <- function(request = NULL,
   # capture supplied arguments
   args <- as.list(environment())
   
+  # handle type correctly
+  if(!is.null(args$request)){
+    args$request$type <- "species"
+  }else{
+    args <- c(list(type = "species"), args)
+  }
+  
   # convert to `data_request` object
   check_atlas_inputs(args) |>
     collapse() |>
