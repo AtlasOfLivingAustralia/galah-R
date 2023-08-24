d4b9bb041fa5e36e73fd5f2852b1232e59a09565
Dax Kellie <dax.kellie@csiro.au>
2023-08-24 15:02:47 +1000

    Add notes for obsolete `collect_occurrences()` tests

--- tests/testthat/test-collect_occurrences.R
+++ tests/testthat/test-collect_occurrences.R
@@ -1,3 +1,5 @@
+## FIXME: collect_occurrences() is no longer the function to download data with a doi
+## Update to new method
 test_that("collect_occurrences downloads data from a DOI", {
   skip_on_cran()
     doi <- "10.26197/ala.0c1e8744-a639-47f1-9a5f-5610017ba060"
@@ -6,6 +8,10 @@ test_that("collect_occurrences downloads data from a DOI", {
     expect_gt(nrow(occ), 0)
 })
 
+## FIXME: collect_occurrences() is no longer the function to do this with
+## Update to new method
 test_that("collect_occurrences checks DOI provided", {
   expect_error(collect_occurrences(doi = "random_doi"))
 })
+
+# Future: collect_occurrences provides a deprecated function message
