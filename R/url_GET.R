3a0952260666f8e0fefdbaa92a5a1ada37396dcc
dewmini <5234594+dewmini@users.noreply.github.com>
2023-08-25 11:23:49 +1000

    Fix error

--- R/url_GET.R
+++ R/url_GET.R
@@ -11,7 +11,7 @@ url_GET <- function(url,
 
   cli <- HttpClient$new(
     url = url,
-    headers = list("User-Agent" = galah_version_string(), "x-api-key" = getOption("galah_config")$user$apikey,))
+    headers = list("User-Agent" = galah_version_string(), "x-api-key" = getOption("galah_config")$user$apikey))
 
   # workaround for fq troubles
   if (length(params$fq) > 1) {
