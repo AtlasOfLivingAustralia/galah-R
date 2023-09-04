b39d27ec5385c94a23ed78db52d63e348b8828c7
dewmini <5234594+dewmini@users.noreply.github.com>
2023-09-04 15:37:48 +1000

    Fix errors

--- R/api_authenticate.R
+++ R/api_authenticate.R
@@ -9,19 +9,19 @@ api_authenticate <- function() {
     apikey <- tokens$value[which(tokens$token == "apikey")[1]]
     refresh_token <- tokens$value[which(tokens$token == "refresh_token")[1]]
 
-    if(!is.null(access_token)) {
-        refresh_url <- "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
-              req_params <- list(
-                refresh_token = "",
-                client_id = "hs79ejhce04n1vms1kju7ejqf",
-                grant_type = "refresh_token",
-                client_secret = NULL
-              )
-              response <- POST(refresh_url, body = req_params, encode = "form")
-              refresh_data <- content(response)
-              print(refresh_data)
-    }
-    else{
+#     if(!is.null(access_token)) {
+#         refresh_url <- "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
+#               req_params <- list(
+#                 refresh_token = "",
+#                 client_id = "hs79ejhce04n1vms1kju7ejqf",
+#                 grant_type = "refresh_token",
+#                 client_secret = NULL
+#               )
+#               response <- POST(refresh_url, body = req_params, encode = "form")
+#               refresh_data <- content(response)
+#               print(refresh_data)
+#     }
+#     else{
         endpoint <- oauth_endpoint(
             authorize = "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize",
             access = "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
@@ -50,7 +50,7 @@ api_authenticate <- function() {
         string2 <- access_token
         header <- paste(string1, string2)
 
-    }
+#     }
 
     cli <- HttpClient$new(
         url = "https://api.test.ala.org.au/common/api/getApikey",
