d9f38942e4f9e5abcbe9ea29b591a2f0a7897369
dewmini <5234594+dewmini@users.noreply.github.com>
2023-09-07 11:43:06 +1000

    Add logs

--- R/api_authenticate.R
+++ R/api_authenticate.R
@@ -12,6 +12,8 @@ api_authenticate <- function() {
 
     if(!is.null(access_token)) {
         tokenData <- jwt_split(access_token)
+        print(tokenData$payload$exp)
+        print(as.numeric(as.POSIXct(Sys.Date())))
         if(as.numeric(as.POSIXct(Sys.Date())) > tokenData$payload$exp){
               refresh_url <- "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
               req_params <- list(
