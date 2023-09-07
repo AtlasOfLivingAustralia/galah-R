2f03a6c6a6db09ec2be2cb84c2561554c6a3551f
dewmini <5234594+dewmini@users.noreply.github.com>
2023-09-08 08:25:56 +1000

    Fix refresh token related issues

--- R/api_authenticate.R
+++ R/api_authenticate.R
@@ -63,6 +63,7 @@ collect_api_keys <- function(client, token){
 #' This function is used to authenticate the user and generate JWT token which 
 #' needs to be passed in API request headers
 # @importFrom httr oauth2.0_token oauth_app oauth_endpoint POST content
+# @importFrom jose jwt_split
 #' @noRd
 #' @keywords Internal
 api_authenticate_original <- function() {
@@ -73,49 +74,53 @@ api_authenticate_original <- function() {
   access_token <- tokens$value[which(tokens$token == "access_token")[1]]
   apikey <- tokens$value[which(tokens$token == "apikey")[1]]
   refresh_token <- tokens$value[which(tokens$token == "refresh_token")[1]]
-  
-  #     if(!is.null(access_token)) {
-  #         refresh_url <- "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
-  #               req_params <- list(
-  #                 refresh_token = "",
-  #                 client_id = "hs79ejhce04n1vms1kju7ejqf",
-  #                 grant_type = "refresh_token",
-  #                 client_secret = NULL
-  #               )
-  #               response <- POST(refresh_url, body = req_params, encode = "form")
-  #               refresh_data <- content(response)
-  #               print(refresh_data)
-  #     }
-  #     else{
-  endpoint <- oauth_endpoint(
-    authorize = "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize",
-    access = "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
-  )
-  app <- oauth_app(
-    "galah",
-    key = "hs79ejhce04n1vms1kju7ejqf",
-    secret = NULL
-  )
-  
-  key_response <- oauth2.0_token(
-    endpoint,
-    app,
-    scope = c("openid","profile","email", "ala/attrs", "ala/roles"),
-    type = "application/json",
-    use_basic_auth = FALSE,
-    config_init = list(),
-    client_credentials = FALSE,
-    credentials = NULL,
-    as_header = TRUE
-  )
-  
-  access_token <- key_response$credentials$access_token
-  refresh_token <-  key_response$credentials$refresh_token
+
+  if(!is.null(access_token)) {
+      tokenData <- jwt_split(access_token)
+      if(as.numeric(as.POSIXct(Sys.time())) > tokenData$payload$exp){
+            refresh_url <- "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
+            req_params <- list(
+              refresh_token = refresh_token,
+              client_id = "5kahrda00sbg0g64su20d8ebkt",
+              grant_type = "refresh_token",
+              client_secret = NULL
+            )
+            response <- POST(refresh_url, body = req_params, encode = "form")
+            refresh_data <- content(response)
+            access_token <- refresh_data$access_token
+      }
+  }
+
+  if(is.null(access_token)){
+      endpoint <- oauth_endpoint(
+          authorize = "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize",
+          access = "https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
+      )
+      app <- oauth_app(
+          "galah",
+          key = "5kahrda00sbg0g64su20d8ebkt",
+          secret = NULL
+      )
+
+      key_response <- oauth2.0_token(
+          endpoint,
+          app,
+          scope = c("openid","profile","email", "ala/attrs", "ala/roles"),
+          type = "application/json",
+          use_basic_auth = FALSE,
+          config_init = list(),
+          client_credentials = FALSE,
+          credentials = NULL,
+          as_header = TRUE
+      )
+
+      access_token <- key_response$credentials$access_token
+      refresh_token <-  key_response$credentials$refresh_token
+  }
+
   string1 <- "Bearer"
   string2 <- access_token
   header <- paste(string1, string2)
-  
-  #     }
   
   cli <- HttpClient$new(
     url = "https://api.test.ala.org.au/common/api/getApikey",
