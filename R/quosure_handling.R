61fde9e95161643714bf0f3cd06cb94b1360d483
Martin Westgate <martinjwestgate@gmail.com>
2023-10-13 15:45:45 +1100

    prevent `%>%` from hiding earlier queries from `filter.data_request()`

--- R/quosure_handling.R
+++ R/quosure_handling.R
@@ -13,7 +13,7 @@ parse_quosures <- function(dots){
   if(length(dots) > 0){
     check_named_input(dots)
     call_string <- deparse(dots[[1]]) |> paste(collapse = " ") # captures multi-lines
-    if(str_detect(call_string, "galah_call()")) {
+    if(str_detect(call_string, "galah_call()|~.")) { # note: "~." indicates presence of the magrittr pipe (%>%)
       eval_request <- eval_tidy(dots[[1]])
       parsed_dots <- lapply(dots[-1], switch_expr_type)
       # check_filter_tibbles(parsed_dots)
