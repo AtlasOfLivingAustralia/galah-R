607a1ef5fadfca38694dae8ff257da0daa0635ff
yasima-csiro <yasima.kankanamge@csiro.au>
2025-10-16 09:12:42 +1100

    Set up JWT token and api key with occurrence status API

--- R/occurrence_status.R
+++ R/occurrence_status.R
@@ -2,7 +2,22 @@
 #' @export
 occurrence_status <- function() {
 
-  result <- url_GET("https://api.test.ala.org.au/common/biocache/occurrences/offline/status")
+   api_authenticate()
+   tokens <- read_csv("./data-raw/tokens.csv")
+   access_token <- tokens$value[which(tokens$token == "access_token")[1]]
+   apikey <- tokens$value[which(tokens$token == "apikey")[1]]
+
+   string1 <- "Bearer"
+   string2 <- access_token
+   auth_header <- paste(string1, string2)
+
+  result <- url_GET("https://api.test.ala.org.au/common/biocache/occurrences/offline/status",
+  add_headers(
+      Authorization = auth_header,
+      "x-api-key" = apikey,
+      Accept = "application/json"
+    )
+  )
   print(result)
   result                             
 }
