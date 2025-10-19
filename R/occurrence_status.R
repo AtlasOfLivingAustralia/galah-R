b1373547439f8b9a958c323d215d61d3d2f264b4
yasima-csiro <yasima.kankanamge@csiro.au>
2025-10-20 10:59:36 +1100

    Update authorization process

--- R/occurrence_status.R
+++ R/occurrence_status.R
@@ -2,22 +2,10 @@
 #' @export
 occurrence_status <- function() {
 
-   api_authenticate()
-   tokens <- read_csv("./data-raw/tokens.csv")
-   access_token <- tokens$value[which(tokens$token == "access_token")[1]]
-   apikey <- tokens$value[which(tokens$token == "apikey")[1]]
-
-   string1 <- "Bearer"
-   string2 <- access_token
-   auth_header <- paste(string1, string2)
-
-  result <- url_GET("https://api.test.ala.org.au/common/biocache/occurrences/offline/status",
-  add_headers(
-      Authorization = auth_header,
-      "x-api-key" = apikey,
-      Accept = "application/json"
-    )
-  )
+   headers = get_auth_headers()
+   result <- url_GET("https://api.test.ala.org.au/common/biocache/occurrences/offline/status",
+      do.call(add_headers, headers)
+   )
   print(result)
   result                             
 }
